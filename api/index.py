b6bb7a23f2c7afd3e577cc07d9a4779be3225156
arcane <tuckeralbert.le.e@gmail.com>
2025-02-06 09:53:35 -0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,9 +2,10 @@ from flask import Flask, request, jsonify
 from g4f.client import Client
 
 app = Flask(__name__)
+
 client = Client()
 
-@app.route('/chat', methods=['GET'])
+@app.route('/chat') # methods=['GET'])
 def chat():
     user_message = request.args.get("message")
     
