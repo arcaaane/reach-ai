9e6ee753568b58c5d6324d71d76bc67514ac0257
arcane <tuckeralbert.le.e@gmail.com>
2025-02-06 09:51:55 -0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from g4f.client import Client
 
 app = Flask(__name__)
+client = Client()
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/chat', methods=['GET'])
+def chat():
+    user_message = request.args.get("message")
+    
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": user_message}],
+        web_search=False
+    )
+    
+    return jsonify({"response": response.choices[0].message.content})
